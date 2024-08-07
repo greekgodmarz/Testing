330496320e60981215706c5b9de92eb405a6af74
Relivian <relivian21@gmail.com>
2024-08-07 12:29:17 +0200

    Java demo added

---
+++ demos/maven/java_lang/src/main/java/com/marz/lang/Basics.java
@@ -0,0 +1,26 @@
+/*
+ * Pick or create your license to be shown here...
+ */
+
+package com.marz.lang;
+
+/**
+ * Class to illustrate basic Java language features
+ * 
+ * @author relivian
+ */
+public class Basics {
+
+  /**
+   * The "main" method is the programs entry point. This method is called when
+   *  a programs is started.
+   * @param args 
+   */
+  public static void main(String[] args) {
+    // print something to the standard output stream (console):
+    System.out.println("Hello World!");
+    
+    // print an error to the error stream:
+    System.err.println("Hello Error!");
+  }
+}
